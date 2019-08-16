6c3ad5cbc3138d7f38a839a4c69cf71ae1193516
NVT45 <nvthuan45@gmail.com>
2019-08-16 19:04:29 +0700

    Done connect database and login form

---
+++ admin/includes/admin_content.php
@@ -0,0 +1,52 @@
+<div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                            Admin
+                            <small>Subheading</small>
+                        </h1>
+                       <?php 
+                           
+                           
+                            // $result_set = User::find_all_users(); nếu đã khai báo static
+                            // while ($row = mysqli_fetch_array($result_set)) {
+                            //     echo $row['username'];
+                            // }
+
+
+                            // $found_user = User::find_user_by_id(1);
+
+                            // $user = User::instantation($found_user);
+                            // echo $user->password;
+
+
+                            // $users = User::find_all_users();
+
+                            // foreach ($users as $user) {
+                            //     echo $user->id;
+                            // }
+
+                            $found_user = User::find_user_by_id(1);
+
+                            echo $found_user->username;
+
+                            
+
+
+
+                        ?>
+                        <ol class="breadcrumb">
+                            <li>
+                                <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
+                            </li>
+                            <li class="active">
+                                <i class="fa fa-file"></i> Blank Page
+                            </li>
+                        </ol>
+                    </div>
+                </div>
+                <!-- /.row -->
+
+            </div>
---
+++ admin/includes/functions.php
@@ -0,0 +1,24 @@
+<?php 
+
+	function classAutoLoader($class){
+		$class = strtolower($class);
+		$the_path = "includes/{$class}.php";
+
+		if (file_exists($the_path)) {
+			require_once($the_path);
+		}
+		else {
+			die ("This file name {$class}.php was not found bro.....");
+		}
+	}
+	spl_autoload_register('classAutoLoader');
+
+
+function redirect($location){
+
+	header("Location: {$location}");
+
+}
+
+
+ ?>
