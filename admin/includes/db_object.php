a6f92818ac43502f4f06c6737d04234283a622f8
NVT45 <nvthuan45@gmail.com>
2019-08-21 16:28:20 +0700

    Admin user function

--- admin/includes/db_object.php
+++ admin/includes/db_object.php
@@ -2,6 +2,39 @@
 
 class Db_object {
 	protected static $db_table = "users";
+	public $errors = array();
+	public $upload_errors_array = array(
+		UPLOAD_ERR_OK => "There is no error, the file uploaded with success",
+		UPLOAD_ERR_INI_SIZE => "The uploaded file exceeds the upload_max_filesize directive", 
+		UPLOAD_ERR_FORM_SIZE => "The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form",
+		UPLOAD_ERR_PARTIAL => "The uploaded file was only partially uploaded",
+		UPLOAD_ERR_NO_FILE =>" No file was uploaded",
+		UPLOAD_ERR_NO_TMP_DIR => "Missing a temporary folder", 
+		UPLOAD_ERR_CANT_WRITE => "Failed to write file to disk",
+		UPLOAD_ERR_EXTENSION => "A PHP extension stopped the file upload"
+
+
+		);
+		
+public function set_file($file){
+
+			if(empty($file) || !$file || !is_array($file)){
+				$this->errors[] = "These was no file uploaded here";
+				return false;
+			}elseif ($file['error'] !=0) {
+				$this->errors[] = $this->upload_errors_array[$file['error']];
+				return false;
+			}
+			else{
+			$this->user_image = basename($file['name']);
+			$this->tmp_path = $file['tmp_name'];
+			$this->type = $file['type'];
+			$this->size = $file['size'];
+
+			}
+
+			
+		}
 	public static function find_all(){
 
 
--- admin/includes/user.php
+++ admin/includes/user.php
@@ -2,16 +2,52 @@
 
 class User extends Db_object{
 		protected static $db_table = "users";
-		protected static $db_table_fields = array('username','password','first_name','last_name');//
+		protected static $db_table_fields = array('username','password','first_name','last_name','user_image');//
 		public $id;
 		public $username;
 		public $password;
 		public $first_name;
 		public $last_name;
+		public $user_image;
+		public $upload_directory = "images";
+		public $image_placeholder = "http://placehold.it/400x400&text=image";
+		
+
+public function image_path_and_placeholder(){
+
+	return empty($this->user_image) ? $this->image_placeholder : $this->upload_directory.DS.$this->user_image;
+}
 
 
+		public function save_user_and_image(){
+			
+					if(!empty($this->errors)){
+						return false;
+					}
 
+					if(empty($this->user_image) || empty($this->tmp_path)){
+						$this->errors[] = "The file was not avainable ";
+						return false;
+					}
 
+					$target_path = SITE_ROOT.DS.'admin'.DS.$this->upload_directory.DS.$this->user_image;
+					if(file_exists($target_path)){
+						$this->errors[]= "The file {$this->user_image} already exists ";
+						return false;
+					}
+					if(move_uploaded_file($this->tmp_path, $target_path)){
+						
+							unset($this->tmp_path);
+							return true;
+						
+
+					}
+					else{
+						$this->errors[]= "The file directory probably does not have permission";
+						return false;
+					}
+
+		}
 public static function verify_user($username, $password){
 
 	global $database;
@@ -26,6 +62,7 @@ public static function verify_user($username, $password){
 	return !empty($the_result_array) ? array_shift($the_result_array) : false;
 
 }
+
 
 	
 }
